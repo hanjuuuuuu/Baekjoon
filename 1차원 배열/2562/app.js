291dc3716e92521ce31bdb85aa75e448831ea594
hanjuuuuuu <hanjucomss@naver.com>
2022-10-14 02:30:55 +0900

    set활용

--- 1차원 배열/2562/app.js
+++ 1차원 배열/2562/app.js
@@ -1,7 +1,7 @@
 //첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다.
 var fs = require('fs')
 var input = fs.readFileSync('./1차원 배열/2562/input.txt').toString().trim().split('\n').map(el => Number(el));
-//var input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');   
+//var input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(el => Number(el));   
 
 let max = input[0];
 let num = 0;
---
+++ 1차원 배열/3052/app.js
@@ -0,0 +1,10 @@
+//42로 나누었을 때, 서로 다른 나머지가 몇 개 있는지 출력한다.
+var fs = require('fs')
+var input = fs.readFileSync('./1차원 배열/3052/input.txt').toString().trim().split('\n').map(Number);
+//var input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);   
+
+const result = input.map((input) => input%42);
+
+// 중복제거
+const set = new Set(result);
+console.log(set.size);
