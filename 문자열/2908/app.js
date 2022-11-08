aea84220aaff30cbe81aa5a33985d49b6dd14cff
hanjuuuuuu <hanjucomss@naver.com>
2022-11-09 02:17:09 +0900

    문자열 if

--- 문자열/2908/app.js
+++ 문자열/2908/app.js
@@ -3,19 +3,9 @@ const fs = require("fs")
 const input = fs.readFileSync('./문자열/2908/input.txt').toString().trim().split(' ');
 //const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
 
-
-// if(input[0][2] != input[1][2]){
-//     console.log(input[0][2]>input[1][2] ? input[0][2] : input[1][2]);
-// }
-// else{   //일의 자리 수가 같은 경우
-//     if(input[0][1] != input[1][1]){
-//         console.log(input[0][1]>input[1][1] ? input[0][1] : input[1][1]);
-//     }
-// }
-
 var one = ''
 var two = ''
-for(let i=2; i>=0; i--){
+for(let i=2; i>=0; i--){    //숫자를 뒤집어서 저장한다.
     one += input[0][i];
     two += input[1][i];
 }
