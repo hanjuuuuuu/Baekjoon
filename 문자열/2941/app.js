4962436fd2193d419b6ac9d542f9c58381d4617c
hanjuuuuuu <hanjucomss@naver.com>
2022-11-18 00:44:50 +0900

    문자열 finish

--- 문자열/2941/app.js
+++ 문자열/2941/app.js
@@ -1,16 +1,19 @@
 //크로아티아 알파벳의 개수를 세는 문제
 const fs = require("fs")
 const input = fs.readFileSync('./문자열/2941/input.txt').toString().trim();
-//let input = fs.readFileSync('/dev/stdin').toString().trim();
+//const input = fs.readFileSync('/dev/stdin').toString().trim();
 
 let answer=input;
 let croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 
 const check = function(answer){
-    for (let i of croatia){
-        answer = answer.replace(i, '.');
+    for(let j=0; j<croatia.length; j++){
+        if(answer.includes(croatia[j])){
+            answer = answer.replace(croatia[j], '.');
+            j--;
+        }
+        console.log(answer)
     }
-    console.log(answer)
     return answer.length;
 }
 console.log(check(answer));
