343b06e4cd42b556c77796dd9d532b77ccd77ff3
hanjuuuuuu <hanjucomss@naver.com>
2022-11-26 23:40:12 +0900

    2869 수정

--- 기본 수학 1/2869/app.js
+++ 기본 수학 1/2869/app.js
@@ -3,18 +3,8 @@ const fs = require("fs")
 const input = fs.readFileSync('./기본 수학 1/2869/input.txt').toString().trim().split(' ');
 //const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
 
-const up = input[0]-input[1];
+var A = input[0];
+var B = input[1];
+var V = input[2];
 
-let day = 0;
-let sum = 0;
-while(input[2]>sum){
-    sum+= up;
-    day++;
-    if(Number(sum)+Number(input[0]) >= Number(input[2])){
-        day++;
-        console.log(sum, Number(sum)+Number(input[0]))
-        console.log('end');
-        break;
-    }
-}
-console.log('답',day);
+console.log(Math.ceil((V-B)/(A-B)));
