6c04098f1f0c4f7d7e98e3afaca6a789de6e1345
hanjuuuuuu <hanjucomss@naver.com>
2022-11-28 15:19:44 +0900

    수정완료

--- 기본 수학 1/10250/app.js
+++ 기본 수학 1/10250/app.js
@@ -3,24 +3,21 @@ const fs = require("fs")
 const input = fs.readFileSync('./기본 수학 1/10250/input.txt').toString().trim().split('\n');
 //const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-console.log(input[0],input[1][0], input[2][1])
-
 const T = input[0];     //테스트 개수
-for (i = 1; i <= T; i++) {
-    let tmp = input[i].split(" ").map(Number);
-    let H = tmp[0];
-    let W = tmp[1];
-    let N = tmp[2];
-
-    num = 0;
-    num = parseInt(Math.ceil(N / H)); 
-
-    if (N % H === 0) {
-      num += H * 100; 
-    } else {
-      num += (N % H) * 100; 
+for(let i=1; i<=T; i++){
+    const [height, width, count] = input[i].split(' ');
+    let share = Math.floor(count/height);      //몫
+    let remainder = count%height;          //나머지
+    if(remainder === 0){
+        share -= 1;
+        remainder = height;
     }
-    result += num + "\n";
-    }
+    const front = remainder;      //방 번호의 앞부분
+    const back= twolen(share);        //방 번호의 뒷부분
+    const room = String(front)+String(back);
+    console.log(room);
+}
 
-    console.log(result);
+function twolen(n) {    //수를 두자리 수로 변환해서 호수 뒷부분을 리턴하는 함수
+    return (n<10 ? '0': '') + parseInt(n+1);
+}
--- 기본 수학 1/2869/app.js
+++ 기본 수학 1/2869/app.js
@@ -3,8 +3,8 @@ const fs = require("fs")
 const input = fs.readFileSync('./기본 수학 1/2869/input.txt').toString().trim().split(' ');
 //const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
 
-var A = input[0];
-var B = input[1];
-var V = input[2];
+var up = input[0];
+var down = input[1];
+var height = input[2];
 
-console.log(Math.ceil((V-B)/(A-B)));
+console.log(Math.ceil((height-down)/(up-down)));
