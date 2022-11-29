89b922fc085d89f4a7885907a58e5c8b3cf1d1df
hanjuuuuuu <hanjucomss@naver.com>
2022-11-29 23:51:40 +0900

    10250 수정완료

--- 기본 수학 1/10250/app.js
+++ 기본 수학 1/10250/app.js
@@ -8,16 +8,16 @@ for(let i=1; i<=T; i++){
     const [height, width, count] = input[i].split(' ');
     let share = Math.floor(count/height);      //몫
     let remainder = count%height;          //나머지
-    if(remainder === 0){
+    if(parseInt(remainder) == 0){
         share -= 1;
         remainder = height;
     }
-    const front = remainder;      //방 번호의 앞부분
-    const back= twolen(share);        //방 번호의 뒷부분
-    const room = String(front)+String(back);
+   // const front = remainder*100;      //방 번호의 앞부분
+    //const back= twolen(share);        //방 번호의 뒷부분
+    const room = remainder*100 + share+1;
     console.log(room);
 }
 
-function twolen(n) {    //수를 두자리 수로 변환해서 호수 뒷부분을 리턴하는 함수
-    return (n<10 ? '0': '') + parseInt(n+1);
-}
+// function twolen(n) {    //수를 두자리 수로 변환해서 호수 뒷부분을 리턴하는 함수
+//     return (n<10 ? '0': '') + parseInt(n+1);
+// }
