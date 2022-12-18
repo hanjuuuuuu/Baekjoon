16ccdd712580ebdbf73e649f1de474d7ab485779
hanjuuuuuu <hanjucomss@naver.com>
2022-12-18 23:59:48 +0900

    골드바흐 파티션 출력 수정 필요

--- 기본 수학 2/9020/app.js
+++ 기본 수학 2/9020/app.js
@@ -3,31 +3,31 @@ const fs = require('fs')
 const input = fs.readFileSync('./기본 수학 2/9020/input.txt').toString().trim().split('\n');
 //const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-// const T = input[0];     //테스트 케이스 수
-// for(let i=1; i<=T; i++){
-//     let n = input[i];
-// }
-
 let primeList = [];
-for(let i=0; i<input.length; i++){
-    let n = parseInt(input[i]);
-    let prime = 0;
+let isPrime = [];
+const T = input[0];     //테스트 케이스 수
+let n = 0;
+for(let i=1; i<=T; i++){
+    n = parseInt(input[i]);
 
-    if(n === 0) break;
-
-    for(let j=2; j<=2*n; j++){          //2부터 2n까지 소수로 설정
+    for(let j=2; j<=n; j++){          //2부터 n까지 소수로 설정
         primeList[j] = true;
     }
-    for(let k=2; k*k<=2*n; k++){        //배수를 소수가 아닌수로 설정
+    for(let k=2; k*k<=n; k++){        //배수를 소수가 아닌수로 설정
         if(primeList[k]){
-            for(let h=k*k; h<=2*n; h+=k){       
+            for(let h=k*k; h<=n; h+=k){       
                 primeList[h] = false;
             }
         }
     }
-    for(let t=n+1; t<=2*n; t++){
+    for(let t=2; t<=n; t++){
         if(primeList[t]){
-            prime++;
+            isPrime.push(t);    //소수만 담아두기
+        }
+    }
+    for(let z=isPrime.length/2; z<isPrime.length; z++){
+        if(isPrime[n-isPrime[z]]){
+            console.log(isPrime[z])
         }
     }
 }
