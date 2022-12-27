9d2c1252d74eafe29829d30d70329183a3c44f0b
hanjuuuuuu <hanjucomss@naver.com>
2022-12-27 15:31:23 +0900

    행렬 2차원배열로 만들기

--- 2차원 배열/2738/app.js
+++ 2차원 배열/2738/app.js
@@ -3,25 +3,26 @@ const fs = require("fs")
 const input = fs.readFileSync('./2차원 배열/2738/input.txt').toString().trim().split('\n');
 //const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const N = parseInt(input[0][0]);
-const M = parseInt(input[0][2]);
-let A = [N+1][M+1];         //행렬 A
-let B = [N+1][M+1];        //행렬 B
+const row = parseInt(input[0][0]);
+const column = parseInt(input[0][2]);
+const A = Array.from(new Array(column), () => new Array(row).fill(0));         //행렬 A
+const B = Array.from(new Array(column), () => new Array(row).fill(0));        //행렬 B
 
-for(let i=1; i<=N; i++){     //행렬 A입력
-    for(let k=0; k<M; k+=2){
-        A[i][k].push(input[i][k]);
+
+for(let i=1; i<=row; i++){     //행렬 A입력
+    for(let k=0; k<column; k+=2){
+        A.push(input[i][k]);
     }
 }
 
-for(let i=1; i<=N; i++){     //행렬 B입력
-    for(let k=0; k<M; k+=2){
-        B[i][k].push(input[i+N][k])
+for(let i=1; i<=row; i++){     //행렬 B입력
+    for(let k=0; k<column; k+=2){
+        B.push(input[i+row][k])
     }
 }
 
-for(let i=1; i<=N; i++){
-    for(let k=0; k<M; k++){
+for(let i=1; i<=row; i++){
+    for(let k=0; k<column; k++){
         console.log(A[i][k] + B[i][k]);
     }
     console.log('\n');
