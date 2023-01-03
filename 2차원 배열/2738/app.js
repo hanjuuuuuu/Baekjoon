7c8175c76826bc6e7a5a990efc9d61c4aad7f348
hanjuuuuuu <hanjucomss@naver.com>
2023-01-03 23:56:47 +0900

    2738 typeerror 수정 중

--- 2차원 배열/2738/app.js
+++ 2차원 배열/2738/app.js
@@ -1,33 +1,28 @@
 //행렬을 2차원 배열로 만들어 더하는 문제
 const fs = require("fs")
-const input = fs.readFileSync('./2차원 배열/2738/input.txt').toString().trim().split('\n');
-//const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = fs.readFileSync('./2차원 배열/2738/input.txt').toString().trim().split('\n').map((x)=>x.trim().split(' ').map((x)=> x));
+//const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map((x)=>x.trim().split(' ').map((x)=> x));
 
-const row = parseInt(input[0][0]);
-const column = parseInt(input[0][2]);
-const A = Array.from(new Array(column), () => new Array(row).fill(0));         //행렬 A
-const B = Array.from(new Array(column), () => new Array(row).fill(0));        //행렬 B
+const [row, column] = input.shift();
 
+var answer = Array.from(new Array(column), () => new Array(row).fill(0)); //2차원배열 A
+//console.log(answer)
 
-for(let i=1; i<=row; i++){     //행렬 A입력
-    var num = input[i].split(' ');
-    console.log(num[0],num[1],num[2],'h')
-    // for(let k=num[0]; k<=column; k++){
-    //     A.push(input[i][k]);
-    //     console.log('a', input[i][k])
-    // }
-}
-
-for(let i=1; i<=row; i++){     //행렬 B입력
-    for(let k=0; k<=(column+1); k+=2){
-        B.push(input[i+row][k])
-        console.log('b',input[i+row][k])
+for(let i=0; i<parseInt(row); i++){
+    console.log('here',parseInt(i+row))
+    for(let j=0; j<parseInt(column); j++){
+        //console.log('here',input[i][j], input[i+row][j])
+        console.log('j',j);
+        answer[i][j] = input[i][j] + input[parseInt(i+row)][j];
+        //console.log(answer[i][j]);
     }
 }
 
-for(let i=1; i<=row; i++){
-    for(let k=0; k<column; k++){
-        console.log(A[i][k] + B[i][k]);
+let array = "";
+for (let i=0; i < row; i++) {
+    for (let j=0; j < column; j++) {
+        array += answer[i][j].toString() + " ";
     }
-    console.log('\n');
+    array += "\n";
 }
+console.log(array);
