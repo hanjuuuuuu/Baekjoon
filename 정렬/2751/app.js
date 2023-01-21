79a4746126fb5daf65779e1003d309161d4efcfd
hanjuuuuuu <hanjucomss@naver.com>
2023-01-21 23:56:26 +0900

    2108 ing

--- 정렬/2751/app.js
+++ 정렬/2751/app.js
@@ -5,7 +5,5 @@ const input = fs.readFileSync('./정렬/2751/input.txt').toString().trim().split
 const N = input.shift();        //N개의 수
 
 const sorted = input.sort((a,b)=> a-b);
-// for(let i=0; i<N; i++){
-//     console.log(sorted[i]);
-// }
-console.log(input.join('\n'))
+
+console.log(input.join('\n'));
