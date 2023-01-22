df6db256f1ff652a1f04f910fa927d608b2f660b
hanjuuuuuu <hanjucomss@naver.com>
2023-01-22 18:21:30 +0900

    최빈값 다시공부

---
+++ 정렬/2108/app.js
@@ -0,0 +1,46 @@
+const { privateDecrypt } = require('crypto');
+const fs = require('fs')
+const input = fs.readFileSync('./정렬/2108/input.txt').toString().trim().split('\n');
+//const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const N = Number(input[0]);     //N개의 수
+input.shift();
+const sorted = input.sort((a,b) => a-b);    //오름차순으로 정렬
+console.log(sorted)
+
+// 산술평균: N개의 수들의 합을 N으로 나눈 값
+function getAverage(array) {
+    let sum = 0;
+    for (let i=0; i<N; i++) {
+        sum += parseInt(array[i]);
+    }
+    return Math.round(sum / N);
+}
+
+// 중앙값: N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값 
+function getMedian(array) {
+    const mid = Math.floor(N / 2);
+    return array[mid];
+}
+
+// 최빈값 : N개의 수들 중 가장 많이 나타나는 값
+function getMode(array) {
+    const count = array.reduce((pv, cv)=> {
+        pv[cv] = (pv[cv] || 0)+1;
+        return pv;
+    }, {});
+}
+
+// 범위 : N개의 수들 중 최댓값과 최솟값의 차이
+function getRange(array) {
+    const min = array[0];
+    const max = array[N-1];
+    
+    return max-min;
+    
+}
+
+console.log(getAverage(sorted));
+console.log(getMedian(sorted));
+console.log(getMode(sorted));
+console.log(getRange(sorted));
