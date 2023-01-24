a47f2a108757dae40151a522371b727879a9bcba
hanjuuuuuu <hanjucomss@naver.com>
2023-01-24 10:42:59 +0900

    sort는 배열을 반환

---
+++ 정렬/1427/app.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const input = fs.readFileSync('./정렬/1427/input.txt').toString().trim();
+//const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+var sorted=[];
+for(let i=0; i<input.length; i++){
+    sorted.push(input[i])
+}
+console.log(sorted.sort((a,b)=>b-a).join(''));
+// for(let i=0; i<sorted.length; i++){
+//     console.log(sorted)
+// }
