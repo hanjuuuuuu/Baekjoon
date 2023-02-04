88659d9aaf37d64da8d9cae0e64d993c6d9240eb
hanjuuuuuu <hanjucomss@naver.com>
2023-02-05 00:51:28 +0900

    좌표압축 공부

--- 정렬/18870/app.js
+++ 정렬/18870/app.js
@@ -1,3 +1,7 @@
 const fs = require('fs');
 const input = fs.readFileSync('./정렬/18870/input.txt').toString().trim().split('\n');
 //const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const N = input.shift();    //N개의 좌표
+
+console.log(input)
