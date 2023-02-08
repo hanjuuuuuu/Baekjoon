c78abf163bb740f4d664f419c38a9c4d13281a0b
hanjuuuuuu <hanjucomss@naver.com>
2023-02-08 23:19:44 +0900

    좌표압축 다시

--- 정렬/18870/app.js
+++ 정렬/18870/app.js
@@ -3,3 +3,12 @@ const input = fs.readFileSync('./정렬/18870/input.txt').toString().trim().spli
 //const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const N = input.shift();    //N개의 좌표
+
+const array = [];
+for(let i=0; i<N; i++){
+    array.push(input[0].split(' ')[i])
+}
+
+console.log(array)
+
+//반복문 돌면서 작은 수 배열에 넣고 배열의 중복값없애기 => 개수를 답 배열에 넣기
--- 정렬/2108/app.js
+++ 정렬/2108/app.js
@@ -5,7 +5,7 @@ const input = fs.readFileSync('./정렬/2108/input.txt').toString().trim().split
 const N = Number(input[0]);     //N개의 수
 input.shift();
 const sorted = input.sort((a,b) => a-b);    //오름차순으로 정렬
-console.log(sorted)
+console.log(input)
 
 // 산술평균: N개의 수들의 합을 N으로 나눈 값
 function getAverage(array) {
@@ -13,7 +13,7 @@ function getAverage(array) {
     for (let i=0; i<N; i++) {
         sum += Number(array[i]);
     }
-    return Math.round(sum/N) === -0 ? Number(0) : Math.round(sum/N);
+    return Math.round(sum/N) === -0 ? 0 : Math.round(sum/N);
 }
 
 // 중앙값: N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값 
@@ -25,7 +25,7 @@ function getMedian(array) {
 // 최빈값 : N개의 수들 중 가장 많이 나타나는 값(여러 개 있을 때에는 최빈값 중 두 번째로 작은 값)
 function getMode(array) {
     const map = new Map();
-    for (let i = 0; i < N; i++) {
+    for (let i = 0; i < N; i++) {   //(숫자, 숫자의 개수) 추가
         if(!map.has(array[i])) {
             map.set(array[i], 1);
         } else {
@@ -34,7 +34,8 @@ function getMode(array) {
     }
     let maxValue = 0;
     let answer = [];
-    map.forEach((element, key) => {
+    map.forEach((element, key) => {     //많이 나온 수를 answer에 push
+        //예제 3 다시 넣어보기
         if (maxValue < element) {
             maxValue = element;
             answer = [];
