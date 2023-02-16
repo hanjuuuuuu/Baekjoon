2c75a893d2ef0ded33fe96a7ead72d986cd023ae
hanjuuuuuu <hanjucomss@naver.com>
2023-02-16 17:42:56 +0900

    좌표 압축 출력변경해야함

--- 정렬/18870/app.js
+++ 정렬/18870/app.js
@@ -5,10 +5,19 @@ const input = fs.readFileSync('./정렬/18870/input.txt').toString().trim().spli
 const N = input.shift();    //N개의 좌표
 
 const array = [];
-for(let i=0; i<N; i++){
+for(let i=0; i<N; i++){     //좌표를 배열에 저장
     array.push(input[0].split(' ')[i])
 }
-
 console.log(array)
 
-//반복문 돌면서 작은 수 배열에 넣고 배열의 중복값없애기 => 개수를 답 배열에 넣기
+//정렬 후 중복제거하고 인덱스 확인하기
+array.sort((a,b)=> {
+    return a-b;
+});
+
+var uniqueArray = [...new Set(array)];
+console.log(uniqueArray)
+
+for(let i=0; i<N; i++){
+    console.log(uniqueArray.indexOf(input[0].split(' ')[i]) )
+}
